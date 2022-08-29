e3e60356b0cb4a83a2901267444f236f6d10b4fa
Wladimir Sidorenko <wlsidorenko@gmail.com>
2022-08-29 23:20:16 +0200

    made first two exercises

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.util.Random
 
-def msg = "I was compiled by Scala 3. :)"
+
+def s99_P01(list: Seq[Int]): Int =
+  list.last
+
+
+def s99_P02(list: Seq[Int]): Int =
+  list.length match
+    case 0 | 1 => -1
+    case length => list(length - 2)
+
+
+@main def hello: Unit =
+  val list = Seq.fill(5)(Random.nextInt)
+  println(s"List $list")
+  var i = 1
+  println(s"Task ${i}: Find the last element of a list: ${s99_P01(list)}")
+  i += 1
+  println(s"Task ${i}: Find the last but one element of a list: ${s99_P02(list)}")
+  i += 1
+  println()
