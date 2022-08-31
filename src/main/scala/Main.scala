98e7f7c1dab36f0090f35679ae6731868275fe24
Wladimir Sidorenko <wlsidorenko@gmail.com>
2022-08-31 08:03:41 +0200

    made more exercises

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,40 @@
 import scala.util.Random
 
 
-def s99_P01(list: Seq[Int]): Int =
-  list.last
+def s99_P02(nums: Seq[Int], idx: Int): Int =
+  if idx < nums.length then
+    return nums(idx)
+  return -1
 
 
-def s99_P02(list: Seq[Int]): Int =
-  list.length match
-    case 0 | 1 => -1
-    case length => list(length - 2)
+def isPalindrome(nums: Seq[Int] | String): Boolean =
+  val middle = nums.length / 2
+  for i <- 0 until middle
+      j <- nums.length - 1 to middle by -1
+  do
+    if nums(i) != nums(j) then
+      return false
+  return true
 
 
 @main def hello: Unit =
-  val list = Seq.fill(5)(Random.nextInt)
-  println(s"List $list")
+  val nums = for i <- 1 to 5 yield Random.nextInt(i)
+  println(s"List $nums")
   var i = 1
-  println(s"Task ${i}: Find the last element of a list: ${s99_P01(list)}")
+  println(s"Task ${i}: Find the last element of a list: ${nums.last}")
   i += 1
-  println(s"Task ${i}: Find the last but one element of a list: ${s99_P02(list)}")
+  println(s"Task ${i}: Find the last but one element of a list: ${s99_P02(nums, nums.length - 2)}")
+  i += 1
+  val k = Random.nextInt(nums.length)
+  println(s"Task ${i}: Find the ${k}-th element of a list: ${s99_P02(nums, k)}")
+  i += 1
+  println(s"Task ${i}: Find the number of elements of a list: ${nums.length}")
+  i += 1
+  println(s"Task ${i}: Reverse a list: ${nums.reverse}")
+  i += 1
+  println(s"Find out whether a list is a palindrome: ${isPalindrome(nums)}")
+  val abba = "abba"
+  println(s"Find out whether 'abba' is a palindrome: ${isPalindrome(abba)}")
+  println(s"Find out whether a list is a palindrome: ${isPalindrome(nums)}")
   i += 1
   println()
