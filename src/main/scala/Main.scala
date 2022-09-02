cf2f30c37ea0c59d023b463ccda1418aaa47cfc0
Wladimir Sidorenko <wlsidorenko@gmail.com>
2022-09-02 17:37:39 +0200

    added P08 and P09

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -21,6 +21,32 @@ def flatten(ls: List[Any]): List[Any] = ls flatMap {
   case e => List(e)
 }
 
+
+def compress[T](ls: Seq[T]): Seq[T] = {
+  var ret: List[T] = Nil
+  var prev: Option[T] = None
+  ls.foreach(
+    (el) => {
+      if prev.isEmpty || el != prev.get then
+        ret = ret ::: List(el)
+        prev = Some(el)
+    }
+  )
+  return ret;
+}
+
+
+def pack[T](ls: List[T]): List[T | List[T]] = {
+  def compressR(result: List[T | List[T]], curList: List[T]): List[T | List[T]] =
+    curList match {
+      case h :: tail if tail.isEmpty || tail.head != h => compressR(h :: result, tail)
+      case h :: tail => compressR((h :: tail.takeWhile(_ == h)) :: result, tail.dropWhile(_ == h))
+      case Nil => result.reverse
+    }
+  compressR(Nil, ls)
+}
+
+
 @main def hello: Unit =
   val nums = for i <- 1 to 5 yield Random.nextInt(i)
   println(s"List $nums")
@@ -44,4 +70,11 @@ def flatten(ls: List[Any]): List[Any] = ls flatMap {
   i += 1
   val numList = List(List(1, 2, 3), 4, List(5, List(6, 7)))
   println(s"Task ${i}: Flatten a nested list structure: ${flatten(numList)}")
+  i += 1
+  val duplicateList = List('a', 'a', 'a', 'a', 'b', 'c', 'c',
+    'a', 'a', 'd', 'e', 'e', 'e', 'e')
+  println(s"Task ${i}: Eliminate consecutive duplicates of list elements.: ${compress(duplicateList)}")
+  i += 1
+  println(s"Task ${i}: Pack consecutive duplicates of list elements into sublists.: ${pack(duplicateList)}")
+  i += 1
   println()
